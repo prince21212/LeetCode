02423730bd57fa13f0d90c0c51b6edfc622353e3
prince kumar <113275797+prince21212@users.noreply.github.com>
2024-06-27 23:38:46 +0530

    Time: 26 ms (17.91%), Space: 47 MB (64.27%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -5,14 +5,20 @@ class Solution {
         int res=0;
         hm.put(0,1);
         for(int i =0; i < nums.length; i++){
+            //sum of all the elemets with its prefix element
             sum += nums[i];
+            //finding the mod value to compare and put into hashmap if not present
             int mod = sum % k;
+            // handling the the case were mod becomes -ve number 
             if(mod < 0){
-                mod = mod % k +k;
+                mod = mod % k +k; // converting the mod into positive number
             }
+            // if mod is present into map increamrnt the result by one 
+            // we can say that there a subarray which is divisible by k present in map
             if(hm.containsKey(mod)){
                 res += hm.get(mod);
             }
+                // storing the mod value and the frequency of that element in map
                 hm.put(mod,hm.getOrDefault(mod,0)+1);
             }
         return res;
