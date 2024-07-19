ffcddd014f3c8b483088fe8c95478de0c1b1be86
prince kumar <113275797+prince21212@users.noreply.github.com>
2024-07-19 21:28:30 +0530

    Time: 2 ms (93.93%), Space: 45.5 MB (6.00%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.java
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.java
@@ -1,29 +1,27 @@
 class Solution {
     public List<Integer> luckyNumbers (int[][] matrix) {
-        List<Integer> row_min=new ArrayList<>();
-        List<Integer> col_max=new ArrayList<>();
-        List<Integer> result=new ArrayList<>();
-        
-        for(int i=0; i<matrix.length; i++){
-            int min=Integer.MAX_VALUE;
-            for(int j=0; j<matrix[0].length; j++){
-                min=Math.min(min, matrix[i][j]);
+        List<Integer> l= new ArrayList<>();
+        int r=matrix.length;
+        int c=matrix[0].length;
+        for(int i=0;i<r;i++){
+              int idx=-1;
+              int min=Integer.MAX_VALUE;
+             int max=Integer.MIN_VALUE;
+            for(int j=0;j<c;j++){
+                if(min>matrix[i][j]){
+                    min=matrix[i][j];
+                    idx=j;
+                }
             }
-            row_min.add(min);
-        }
-        for(int i=0; i<matrix[0].length; i++){
-            int max=Integer.MIN_VALUE;
-            for(int j=0; j<matrix.length; j++){
-                max=Math.max(max, matrix[j][i]);
+            for(int j=0;j<r;j++){
+                if(max<matrix[j][idx]){
+                    max=matrix[j][idx];
+                }
             }
-            col_max.add(max);
+            if(min==max){
+                l.add(min);
+            }
         }
-
-        for(int i=0; i<row_min.size(); i++){
-            if(col_max.contains(row_min.get(i)))
-                result.add(row_min.get(i));
-
-        }
-        return result;
+        return l;
     }
 }
